68c4d37d367b972378fc2d5230535d3e865d6b1c
Eric McCormick <edm00se@gmail.com>
2019-01-01 20:18:19 -0600

    basic random combination implemented

--- cli.js
+++ cli.js
@@ -2,7 +2,8 @@
 
 const chalk = require('chalk');
 const meow = require('meow');
-const { generateRandomFromStartingLetter } = require('./lib');
+const { toTitleCase } = require('./util');
+const { generateRandom } = require('./lib');
 
 const cli = meow(`
   Usage
@@ -18,7 +19,8 @@ const cli = meow(`
 
 const startingLetter = cli.input[0] || '';
 // if( "" !== startingLetter.trim() && startingLetter.length === 1 ){
-  console.log(chalk.yellow.bold(generateRandomFromStartingLetter(startingLetter)));
+  const sprintName = toTitleCase(generateRandom());
+  console.log(chalk.yellow.bold(sprintName));
 // } else {
 //   console.log(chalk.red.bold('A valid starting letter must be specified!'));
 // }
--- lib.js
+++ lib.js
@@ -1,12 +1,32 @@
 const ADJECTIVE_DICTIONARY = require('./adjectives');
 const DINOSAUR_NAMES = require('./dinos');
-const { toTitleCase } = require('./util');
 
 function generateRandomFromStartingLetter(){
   // TODO: logic
-  return toTitleCase('studious stegosaurus');
+  return 'studious stegosaurus';
+}
+
+function generateRandom(){
+  const adj = getRandomAdjective();
+  const dino = getRandomDinosaurName();
+  return `${adj} ${dino.name}`;
+}
+
+function getRandomFromSet(set){
+  return set[Math.floor(Math.random() * set.length)];
+}
+
+function getRandomAdjective(){
+  return getRandomFromSet(ADJECTIVE_DICTIONARY);
+}
+
+function getRandomDinosaurName(){
+  return getRandomFromSet(DINOSAUR_NAMES);
 }
 
 module.exports = {
+  getRandomAdjective,
+  getRandomDinosaurName,
+  generateRandom,
   generateRandomFromStartingLetter
 };
